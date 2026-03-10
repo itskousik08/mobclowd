352a555857c0112a49524c7c314ca2838f719473
itshirdeshkkk <hirdesh.khandelwal@tedekstra.com>
2026-03-10 20:19:48 +0530

    fix: new changes

--- backend/routes/templates.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { TEMPLATES_LIST } = require('../services/templates');
-
-router.get('/', (req, res) => {
-  res.json({ templates: TEMPLATES_LIST });
-});
-
-module.exports = router;
